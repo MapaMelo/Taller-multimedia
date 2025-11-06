76d98da20deb3f550e194c69a8588125c9c4fdcb
MapaMelo <148729836+MapaMelo@users.noreply.github.com>
2025-11-06 18:29:18 +0000

    texto que cambia con click

--- script.js
+++ script.js
@@ -1,5 +1,26 @@
 const titulo = document.getElementById("titulo");
+const textoInicial = document.getElementById("textoInicial");
+let estadoInicial = true;
+let posicionTexto = 0;
+const texto = ["Hola", "bienvenidos", "a", "esta", "pagina", "web"];
+
+textoInicial.innerText = texto[posicionTexto];
+
+textoInicial.addEventListener("click", () => {
+    if (posicionTexto < texto.length - 1) {
+    posicionTexto = posicionTexto + 1;
+    textoInicial.innerText = texto[posicionTexto];
+    } else {
+        posicionTexto = 0;
+        textoInicial.innerText = texto[posicionTexto];
+    }
+})
 
 titulo.addEventListener("click", () => {
-    titulo.innerText = "Bienvenidos";
+    if(estadoInicial === true){
+      titulo.innerText = "Bienvenidos";
+    }else {
+     titulo.innerText = "Spyderman";
+    }
+    estadoInicial = !estadoInicial;
 })
